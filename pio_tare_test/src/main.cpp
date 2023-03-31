2db7a3e5a965468f50b59fb59a199e7f85eb51da
Christophe-A <christo-chibougamo@hotmail.com>
2023-03-31 15:56:26 -0400

    VIP main, MQTT test version.

---
+++ pio_tare_test/src/main.cpp
@@ -0,0 +1,34 @@
+#include <Arduino.h>
+#include <time.h>
+#include <ArduinoLowPower.h>
+
+#include "npt_time.h"
+#include "imu.h"
+#include "load.h"
+#include "data_handling.h"
+
+unsigned long startMillis;
+const char sensor_name[] = "demo_load_cell";
+
+void setup() {
+    setup_data_handler();
+    setup_time();
+    setup_loadcell();
+    setup_imu();
+}
+
+String load_container;
+String imu_container;
+
+void loop() {
+    LoadValue lv;
+    lv.raw = 0.45f;
+    lv.kg = 1.5f;
+    lv.newt = 2.6f;
+
+    format_data_load(&load_container, get_timestamp_now(), sensor_name, &lv);
+    Serial.println(load_container);
+    send_data(load_container, topic_load);
+
+    delay(1000); // sleep 1 sec*/
+}
