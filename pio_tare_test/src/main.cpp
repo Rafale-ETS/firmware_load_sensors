f2810db630c9573ae0cdb9bbc82983abe6e0df67
Christophe-A <christo-chibougamo@hotmail.com>
2023-04-13 15:19:43 -0400

    proper active status

--- pio_tare_test/src/main.cpp
+++ pio_tare_test/src/main.cpp
@@ -17,7 +17,7 @@ String imu_container;
 String status_container;
 LoadValue lval;
 ImuData idata;
-StatusValue sv;
+StatusValue sv; // statuses[0] => general status, statuses[1] => error 1, statuses[2] => error 2 etc...
 
 extern bool tare_request;
 
@@ -31,6 +31,11 @@ void setup() {
     setup_imu(&bno_imu);
     setup_status();
     
+    sv.statuses = (char**)malloc(sizeof(char*)); // allocate 1 status slot
+    sv.statuses[0] = (char*)malloc(strlen(STATUS_STARTING)); // alocate the string to the slot
+    strcpy(sv.statuses[0], STATUS_STARTING); // fill the slot
+    sv.nb_statuses = 1;
+
     delay(STARTUP_DELAY);
 }
 
@@ -38,8 +43,12 @@ void loop() {
     unsigned long start_millis = millis();
     loop_count = loop_count + 1;
 
-    if(tare_request)
+    if(tare_request){
+        ch_status(&sv, 0, STATUS_TARE);
         tare();
+    }
+
+    ch_status(&sv, 0, STATUS_ACTIVE);
 
     read_cell_no_block(&lval);
     Serial.print("Load-Cell: "); Serial.println(lval.raw);
@@ -55,7 +64,6 @@ void loop() {
     // Send status info only every 10 seconds (10 loop)
     //if(loop_count >= 10) {
     loop_count = 0;
-    //add_status(&sv, STATUS_ACTIVE);
 
     read_status_no_block(&sv);
     format_data_status(&status_container, get_timestamp_now(), SENSOR_NAME, &sv);
@@ -66,9 +74,9 @@ void loop() {
     unsigned long end_millis = millis();
     unsigned long delta_millis = LOOP_SLEEP - (end_millis - start_millis);
     if(delta_millis > LOOP_SLEEP) delta_millis = 0;
-    //LowPower.idle(delta_millis); // sleep the full delay, minus the time it took to run the loop.
     Serial.print("Sleeping "); Serial.print(delta_millis); Serial.print(" out of "); Serial.print(LOOP_SLEEP); Serial.println(" millis.");
     
     mqttClient.loop();
-    delay(delta_millis); // temp because LowPower doesn't work.
+    LowPower.sleep(delta_millis); // sleep the full delay, minus the time it took to run the loop.
+    //delay(delta_millis); // temp to keep the usb connection
 }
